fac483d857d438b8dd65d86220fb2c01bfa643d5
Laurent Coustet <laurent.coustet@clarisys.fr>
2018-05-25 15:37:51 +0200

    pysmbc => pysmbc-binary package name for pypi

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -4,30 +4,39 @@ set -e -x
 # Install a system package required by our library
 yum install -y gettext
 
-# Build samba > 3.2 (Samba4 is too much of a pain to build on such old distribution)
+PACKAGE_NAME="pysmbc-binary"
+
+# Build samba
 mkdir -p /io/samba
 (
 	cd /io/samba
-	curl https://download.samba.org/pub/samba/stable/samba-3.6.25.tar.gz -o samba-3.6.25.tar.gz
-	tar xf samba-3.6.25.tar.gz
-	cd samba-3.6.25/source3
-	./configure
-	make
+	yum install -y libacl-devel e2fsprogs-devel readline-deve gdb gcc gcc-c++ zlib-devel
+	curl https://download.samba.org/pub/samba/stable/samba-4.8.2.tar.gz -o samba-4.8.2.tar.gz
+	tar xf samba-4.8.2.tar.gz
+	cd samba-4.8.2
+	PATH=/opt/python/cp27-cp27m/bin/:$PATH
+	./configure --prefix=/usr/local --disable-python --without-ad-dc --enable-fhs --without-ldap --without-ads --without-pam
+	make -j4
 	make install
 	install -m644 pkgconfig/*.pc /usr/local/lib/pkgconfig/
 )
 
 (
 	cd /io/pysmbc
+	# Replace the package name
+	if [[ "${PACKAGE_NAME:-}" ]]; then
+		sed -i 's/name="pysmbc"/name="pysmbc-binary"/' setup.py
+	fi
 	# Compile wheels
+	LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 	for PYBIN in /opt/python/*/bin; do
 	    "${PYBIN}/python" setup.py bdist_wheel --dist-dir /io/wheelhouse/
 	done
 
 	# Bundle external shared libraries into the wheels
-	for whl in wheelhouse/*.whl; do
+	for whl in /io/wheelhouse/*.whl; do
 	    auditwheel repair "$whl" -w /io/wheelhouse/
 	done
-	sha1sum wheelhouse/*.whl
-	ls -la wheelhouse/
+	sha1sum /io/wheelhouse/*.whl
+	ls -la /io/wheelhouse/
 )
